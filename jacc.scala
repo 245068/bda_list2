a017cf45bb09eb3720bbb75a3983679b8c95d9dc
245068 <92883098+245068@users.noreply.github.com>
2021-12-19 22:40:00 +0100

    Add files via upload

---
+++ jacc.scala
@@ -0,0 +1,35 @@
+import scala.collection.mutable.Set
+object js {
+
+def main(args: Array[String]): Unit = {
+	val k = args(2).toInt
+	var bookfile1 = scala.io.Source.fromFile(args(0))
+	var bookfile2 = scala.io.Source.fromFile(args(1))
+	var text = try bookfile1.mkString finally bookfile1.close()
+	var texttoadd = text.toLowerCase().replaceAll("\\p{Punct}", "")
+	var wordset1 = texttoadd.split("\\s+")
+	text = try bookfile2.mkString finally bookfile2.close()
+	texttoadd = text.toLowerCase().replaceAll("\\p{Punct}", "")
+	var wordset2 = texttoadd.split("\\s+")
+
+	var ksh1 = Set[String]()
+	var ksh2 = Set[String]()
+
+	for(i <- 0 to wordset1.length - k){
+		var arr = ""
+		for (j <- 0 to k-1) arr = arr + " " + wordset1(i+j)
+		ksh1 += arr
+	}
+	for(i <- 0 to wordset2.length - k){
+		var arr = ""
+		for (j <- 0 to k-1) arr = arr + " " + wordset2(i+j)
+		ksh2 += arr
+	}
+	//ksh1.foreach(println)
+	//ksh2.foreach(println)
+	var inter = (ksh1&ksh2).size.asInstanceOf[Double]
+	var union = (ksh1|ksh2).size.asInstanceOf[Double]
+	var jaccsim = inter/union
+	println(inter,union,jaccsim)
+}
+}
